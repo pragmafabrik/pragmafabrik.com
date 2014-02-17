5bacb591268932ab9da2404cf8d3e90fa5f4a8f1
Grégoire HUBERT <hubert.greg@gmail.com>
2014-02-17 16:27:51 +0100

    Integrated JC index & layout

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php #web/index.php
 
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
 $app = require dirname(__DIR__).'/sources/application.php';
 
 $app->run();
